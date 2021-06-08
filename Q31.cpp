6a5c0e360fb4db2f8728da11b08c8ce51bb3dd6a
RutujaDudhbhate <80782613+RutujaDudhbhate@users.noreply.github.com>
2021-06-08 23:40:03 +0530

    Add files via upload

---
+++ Q31.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+ 
+
+void Union(int a1[], int a2[], int m, int n)
+{
+    int i = 0, j = 0;
+    while (i < m && j < n) {
+        if (a1[i] < a2[j])
+            cout << a1[i++] << " ";
+ 
+        else if (a2[j] < a1[i])
+            cout << a2[j++] << " ";
+ 
+        else {
+            cout << a2[j++] << " ";
+            i++;
+        }
+    }
+ 
+    
+    while (i < m)
+        cout << a1[i++] << " ";
+ 
+    while (j < n)
+        cout << a2[j++] << " ";
+}
+ 
+
+int main()
+{
+    int a1[] = { 1, 3,4,5,7 };
+    int a2[] = { 2, 3, 5, 6 };
+ 
+    int m = sizeof(a1) / sizeof(a1[0]);
+    int n = sizeof(a2) / sizeof(a2[0]);
+ 
+   
+    Union(a1, a2, m, n);
+ 
+    return 0;
+}
